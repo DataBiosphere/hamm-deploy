99c7cf5e95b8ce3f452bb05fec022dff4ee93855
Qi Wang <qi@broadinstitute.org>
2019-03-25 22:20:27 +0800

    remove opendj

--- init-containers/opendj/opendj/liveness.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# If any commands in this file return non-zero, then this container is not ready
-set -e
-
-# Check that opendj is up and responsive and has the "proxy user" loaded up
-ldapsearch -H ldap://localhost -D "cn=Directory Manager" -w $(cat ${DIR_MANAGER_PW_FILE}) -b "cn=proxy-ro,ou=people,dc=dsde-dev,dc=broadinstitute,dc=org"
